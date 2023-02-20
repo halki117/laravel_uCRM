cb38bca961a1c452a28d67201ece627aed4e0ead
halki117 <s.halki0203@icloud.com>
2023-02-21 08:51:53 +0900

    バリデーション

--- resources/js/Pages/Inertia/Create.vue
+++ resources/js/Pages/Inertia/Create.vue
@@ -2,6 +2,11 @@
 import { reactive } from 'vue';
 import { Inertia } from '@inertiajs/inertia';
 
+// コントローラー側でバリデーションに引っかかった場合errorsという変数が渡ってくる
+defineProps({
+    errors: Object
+})
+
 const form = reactive({
     title: null,
     content: null,
@@ -16,7 +21,11 @@ const submitFunction = ()=>{
 <template>
     <form @submit.prevent="submitFunction">
         <input type="text" name="title" v-model="form.title"><br>
+        <!-- バリデーションメッセージの表示 -->
+        <div v-if="errors.title">{{ errors.title }}</div>
         <input type="text" name="content" v-model="form.content"><br>
+        <!-- バリデーションメッセージの表示 -->
+        <div v-if="errors.content">{{ errors.content }}</div>
         <button>送信</button>
     </form>
 </template>
